8d05afe9348e349eb1552ef7c9d720cbe6b56ade
Aswin <aswin.balasubramanian@agnity.com>
2023-12-09 21:32:15 +0530

    auto wired annotations wiring

---
+++ src/main/java/com/aswin/spring/springCoreAdvanced/autowiring/annotations/Employee.java
@@ -0,0 +1,27 @@
+package com.aswin.spring.springCoreAdvanced.autowiring.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+
+	private Address address;
+
+	public Employee(Address address) {
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	@Autowired
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+
+}
