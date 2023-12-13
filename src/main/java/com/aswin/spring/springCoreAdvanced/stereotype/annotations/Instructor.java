d5d1b1f4bf1cdd84a13ff87613fd27f304aeaa95
Aswin <aswin.balasubramanian@agnity.com>
2023-12-14 01:31:48 +0530

    using value expressions

--- src/main/java/com/aswin/spring/springCoreAdvanced/stereotype/annotations/Instructor.java
+++ src/main/java/com/aswin/spring/springCoreAdvanced/stereotype/annotations/Instructor.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class Instructor {
 
-	@Value("01")
+	@Value("#{66+44}")
 	private int id = 10;
 
 	@Value("#{topics}")
