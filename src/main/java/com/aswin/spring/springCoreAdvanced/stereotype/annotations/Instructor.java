6de80e933669c57ef48ff458181eb229a74e8dcd
Aswin <aswin.balasubramanian@agnity.com>
2023-12-14 01:40:12 +0530

    creating objects using @Value

--- src/main/java/com/aswin/spring/springCoreAdvanced/stereotype/annotations/Instructor.java
+++ src/main/java/com/aswin/spring/springCoreAdvanced/stereotype/annotations/Instructor.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class Instructor {
 
-	@Value("#{T(java.lang.Math).abs(-99)}")
+	@Value("#{new Integer(99)}")
 	private int id = 10;
 
 	@Value("#{topics}")
