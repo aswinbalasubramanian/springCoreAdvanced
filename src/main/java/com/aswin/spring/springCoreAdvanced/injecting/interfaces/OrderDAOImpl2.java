13b5c74491373bb857c8b0f715e7449fde29fc26
Aswin <aswin.balasubramanian@agnity.com>
2023-12-14 02:54:34 +0530

    switching impl using config

---
+++ src/main/java/com/aswin/spring/springCoreAdvanced/injecting/interfaces/OrderDAOImpl2.java
@@ -0,0 +1,11 @@
+package com.aswin.spring.springCoreAdvanced.injecting.interfaces;
+
+public class OrderDAOImpl2 implements OrderDAO {
+
+	@Override
+	public void createOrder() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside createOrder DAO2 Impl");
+	}
+
+}
